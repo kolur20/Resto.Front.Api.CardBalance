f3d6c36a5cf5bb4307f137f9b6af9cc56d756c95
Кирилл Гринь <kolur_20@hotmail.com>
2020-10-15 14:48:33 +0300

    Добавьте файлы проекта.

---
+++ Resto.Front.Api.CardBalance/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Resto.Front.Api.CardBalance
+{
+    public class Class1
+    {
+    }
+}
